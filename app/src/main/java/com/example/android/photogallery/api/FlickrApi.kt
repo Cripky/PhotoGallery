e7922ae82c1caf226eebc07f423e25649b8ac7e2
PC <maxpesherov@gmail.com>
2023-10-09 07:29:39 +0500

    09.10.2023

---
+++ app/src/main/java/com/example/android/photogallery/api/FlickrApi.kt
@@ -0,0 +1,11 @@
+package com.example.android.photogallery.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+
+    @GET("/")
+    fun fetchContents(): Call<String>
+
+}
